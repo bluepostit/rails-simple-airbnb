f093ba2ca5b36985d96a8ff41bc2e402c39a6e1f
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-11-18 18:26:58 +0200

    basic implementation; all except images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Great Manchester Flat',
+  address: '45 Main Ave',
+  description: 'Breezy and chill.',
+  price_per_night: 55,
+  number_of_guests: 4
+)
